d50a01d16f3fede5ef78678e377e8291639f78bd
olegopro <evilgazz@gmail.com>
2022-07-26 19:14:43 +0300

    Создал библиотеку в codeigniter для Oauth2

---
+++ app/Libraries/Oauth.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Libraries;
+
+use OAuth2\GrantType\UserCredentials;
+use OAuth2\Server;
+use OAuth2\Storage\Pdo;
+
+class Oauth
+{
+	public $server;
+
+	public function __construct()
+	{
+		$this->init();
+	}
+
+	public function init()
+	{
+		$dsn = getenv('database.default.DSN');
+		$username = getenv('database.default.username');
+		$password = getenv('database.default.password');
+
+		$storage = new Pdo([
+			'dsn'      => $dsn,
+			'username' => $username,
+			'password' => $password
+		]);
+		
+		$this->server = new  Server($storage);
+		$this->server->addGrantType(new UserCredentials($storage));
+	}
+}
