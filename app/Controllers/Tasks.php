786752cd093edd0c717aabb4443d0da9341ec888
olegopro <evilgazz@gmail.com>
2022-08-03 16:15:09 +0300

    Сделал restful контроллер и модель для tasks

---
+++ app/Controllers/Tasks.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class Tasks extends ResourceController
+{
+
+	public function index()
+	{
+		//
+	}
+
+	public function show($id = null)
+	{
+		//
+	}
+
+	public function new()
+	{
+		//
+	}
+
+	public function create()
+	{
+		//
+	}
+
+
+	public function edit($id = null)
+	{
+		//
+	}
+
+	public function update($id = null)
+	{
+		//
+	}
+
+
+	public function delete($id = null)
+	{
+		//
+	}
+}
---
+++ app/Models/TasksModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class tasksModel extends Model
+{
+	protected $table = 'tasks';
+	protected $primaryKey = 'task_id';
+	protected $allowedFields = ['task_fio', 'task_telephone', 'task_summa', 'task_status'];
+}
