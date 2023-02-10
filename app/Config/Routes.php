0c45615afe068501d3b9185828284166c738e694
olegopro <evilgazz@gmail.com>
2023-02-10 06:44:04 +0300

    Очистил от неиспользуемых компонентов

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -25,7 +25,7 @@ $routes->set404Override();
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
 // Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
-$routes->setAutoRoute(true);
+// $routes->setAutoRoute(false);
 
 /*
  * --------------------------------------------------------------------
@@ -35,12 +35,11 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-$routes->resource('blog');
-$routes->resource('mailregister');
-$routes->resource('vkontaktebot');
+// $routes->get('/', 'Home::index');
 
-//$routes->cli('demo/open/(:segment)', 'Demo::open/$1');
+$routes->resource('blog');
+$routes->resource('MailRegister');
+
 
 /*
  * --------------------------------------------------------------------
